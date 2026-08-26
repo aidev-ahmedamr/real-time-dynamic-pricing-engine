b181c7001493815cbe4e3c6a3de4a5b167c51140
Ahmed Amr <aidev.ahmedamr@gmail.com>
2026-08-26 12:58:51 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,14 +2,21 @@ from fastapi import FastAPI, HTTPException
 import joblib
 import pandas as pd
 
+from api.schemas import (
+    ProductPricingRequest,
+    PricingResponse
+)
+
 from src.optimization.pricing_pipeline import (
     generate_pricing_decision
 )
+
 
 app = FastAPI(
     title="Real-Time Dynamic Pricing Engine",
     version="1.0.0"
 )
+
 
 model = joblib.load(
     "models/demand_model.pkl"
@@ -22,31 +29,29 @@ features = joblib.load(
 
 @app.get("/")
 def root():
-
     return {
-        "message": (
-            "Dynamic Pricing Engine is running"
-        )
+        "message": "Dynamic Pricing Engine is running"
     }
 
 
 @app.get("/health")
 def health():
-
     return {
         "status": "healthy"
     }
 
 
-@app.post("/optimize-price")
+@app.post(
+    "/optimize-price",
+    response_model=PricingResponse
+)
 def optimize_price_endpoint(
-    product_data: dict
+    product_data: ProductPricingRequest
 ):
 
     try:
-
         decision, _ = generate_pricing_decision(
-            product_data,
+            product_data.model_dump(),
             model,
             features
         )
@@ -54,7 +59,6 @@ def optimize_price_endpoint(
         return decision
 
     except Exception as e:
-
         raise HTTPException(
             status_code=500,
             detail=str(e)
